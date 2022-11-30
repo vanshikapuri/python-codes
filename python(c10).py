19b4097e4fda0c4fbe5328f7e7dd98c4aef7fe37
vanshikapuri <119493046+vanshikapuri@users.noreply.github.com>
2022-11-30 15:43:15 +0530

    Add files via upload

---
+++ python(c10).py
@@ -0,0 +1,25 @@
+#trignometric equations
+
+import math
+degree=[0, 30, 45, 60, 90]
+
+for i in degree:
+    radians=math.radians(i)
+print(math.sin(radians))
+
+
+#mathematical constants
+
+import math
+print("pi: ",math.pi)
+print("e: ",math.e)
+print("tau: ",math.tau)
+print("inf: ",math.inf)
+print("nan: ",math.nan)
+
+#functions
+
+def add(a,b):
+    print(a+b)
+
+add(2,4)
